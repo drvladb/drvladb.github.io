59e0238f5877e7a345577a819acab4a1aa1a337d
vlad b <33793511+drvladb@users.noreply.github.com>
2019-10-30 14:08:50 -0400

    added hax

--- game/js/main.js
+++ game/js/main.js
@@ -63,6 +63,38 @@ $(document).ready(function() {
       debugmode = true;
    if(window.location.search == "?easy")
       pipeheight = 200;
+   if(window.location.search == "?hax"){
+      var pipe_gap = 90,
+	flap_thresh = 0;
+
+	function targetHeight() {
+		var p = pipes[0];
+		if (p === undefined) {
+			return ($('#ceiling').offset().top + $('#land').offset().top) / 2;
+		}
+		p = p.children('.pipe_upper');
+		var result = ((p.offset().top + p.height())) + pipe_gap / 2;
+		result += pipe_gap / 8;
+		return result;
+	}
+
+	function currentHeight() {
+		return $('#player').offset().top + $('#player').height() / 2;
+	}
+
+	function flap() {
+		$(document).mousedown();
+		$(document).trigger('touchstart');
+	}
+
+	function decide() {
+		if ((currentHeight() - targetHeight()) > flap_thresh) {
+			flap();
+		}
+	}
+
+window.setInterval(decide, 20);
+   }
    
    //get the highscore
    var savedscore = getCookie("highscore");
